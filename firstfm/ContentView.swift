66a0823f36306e849126f7fd15e27abec010362e
Stanislas Lange <stanislas.lange@pm.me>
2021-06-11 14:35:06 +0200

    Initial Commit

---
+++ firstfm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firstfm
+//
+//  Created by Stanislas Lange on 11/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
