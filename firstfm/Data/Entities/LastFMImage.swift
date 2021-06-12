613f78fa63403b1d6902eb90b5436b80e260843b
Stanislas Lange <stanislas.lange@pm.me>
2021-06-12 22:29:22 +0200

    feat: get artist images from Spotify API

---
+++ firstfm/Data/Entities/LastFMImage.swift
@@ -0,0 +1,18 @@
+//
+//  LastFMImage.swift
+//  firstfm
+//
+//  Created by Stanislas Lange on 12/06/2021.
+//
+
+import Foundation
+
+struct LastFMImage: Codable {
+    var id: String { url }
+    var url: String
+    var size: String
+    
+    private enum CodingKeys : String, CodingKey {
+        case url = "#text", size
+    }
+}
--- firstfm/Data/ViewModel/ChartViewModel.swift
+++ firstfm/Data/ViewModel/ChartViewModel.swift
@@ -11,17 +11,17 @@ import Foundation
 class ChartViewModel: ObservableObject {
     @Published var artists: [Artist] = []
     var isLoading = true
-
+    
     func getChartingArtists() {
         self.isLoading = true
-
+        
         var request = URLRequest(url: URL(string: "https://ws.audioscrobbler.com/2.0/?format=json")!)
-        let data : Data = "api_key=d404c94c63e190519d70002332f09509&method=chart.getTopArtists&limit=15".data(using: .utf8)!
-
+        let data : Data = "api_key=d404c94c63e190519d70002332f09509&method=chart.getTopArtists&limit=10".data(using: .utf8)!
+        
         request.httpMethod = "POST"
         request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField:"Content-Type");
         request.httpBody = data
-
+        
         URLSession.shared.dataTask(with: request) { (data, response, error) -> Void in
             do {
                 if let response = response {
@@ -37,12 +37,29 @@ class ChartViewModel: ObservableObject {
                 }
                 if let data = data {
                     do{
-                        let jsonResponse = try JSONDecoder().decode(ArtistResponse.self, from: data)
-
-                        DispatchQueue.main.async {
-                            self.artists = jsonResponse.artists.artist
-                            self.isLoading = false
+                        var jsonResponse = try JSONDecoder().decode(ArtistResponse.self, from: data)
+                        
+                        let myGroup = DispatchGroup()
+                        
+                        for (index, artist) in jsonResponse.artists.artist.enumerated() {
+                            myGroup.enter()
+                            getImageForArtist(artistName: artist.name) { imageURL in
+                                if let imageURL = imageURL {
+                                    jsonResponse.artists.artist[index].image[0].url = imageURL
+                                    print(jsonResponse)
+                                    myGroup.leave()
+                                }
+                            }
                         }
+                        
+                        myGroup.notify(queue: .main) {
+                            print("Finished all requests.")
+                            DispatchQueue.main.async {
+                                self.artists = jsonResponse.artists.artist
+                                self.isLoading = false
+                            }
+                        }
+                        
                     }
                 }
             }
@@ -51,5 +68,46 @@ class ChartViewModel: ObservableObject {
                 // TODO
             }
         }.resume()
+    }
+}
+
+func getImageForArtist(artistName: String, completion: @escaping (String?) -> ()) {
+    if let encodedArtistName = artistName.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+        let queryURLString = "https://api.spotify.com/v1/search?q=\(encodedArtistName)&type=artist&limit=1"
+        if let queryURL = URL(string: queryURLString) {
+            var request = URLRequest(url: queryURL)
+            
+            request.setValue("application/json", forHTTPHeaderField:"Content-Type");
+            request.setValue("Bearer xxxx", forHTTPHeaderField:"Authorization");
+            
+            URLSession.shared.dataTask(with: request , completionHandler : { data, response, error in
+                do {
+                    if let response = response {
+                        let nsHTTPResponse = response as? HTTPURLResponse
+                        if let statusCode = nsHTTPResponse?.statusCode {
+                            print ("spotify status code = \(statusCode)")
+                        }
+                        // TODO
+                    }
+                    if let error = error {
+                        print (error)
+                        // TODO
+                        completion("")
+                    }
+                    if let data = data {
+                        do{
+                            print(data)
+                            let jsonResponse = try JSONDecoder().decode(SpotifyArtistSearchResponse.self, from: data)
+                            
+                            completion(jsonResponse.artists.items[0].images[0].url)
+                        }
+                    }
+                }
+                catch {
+                    print(error)
+                    // TODO
+                }
+            }).resume()
+        }
     }
 }
