ebdb4c21c7e8521979e99cdbeaace61365004bac
Stanislas Lange <stanislas.lange@pm.me>
2021-06-16 00:24:03 +0200

    feat(charts): init segmented picker

--- firstfm/Views/CharList.swift
+++ firstfm/Views/CharList.swift
@@ -10,27 +10,41 @@ import SwiftUI
 struct ChartList: View {
     @ObservedObject var charts = ChartViewModel()
     @State var loaded = false
+    @State private var selectedChartsIndex = 0
 
     var body: some View {
         NavigationView {
             ZStack {
-                List(charts.artists) { artist in
-                    NavigationLink(
-                        destination: ArtistView(artist: artist),
-                        label: {
-                            ArtistRow(artist: artist)
-                        })
-                }.navigationTitle("Artist charts").onAppear {
-                    if !loaded {
-                        self.charts.getChartingArtists()
-                        // Prevent loading artits again when navigating
-                        self.loaded = true
+                VStack {
+                    Picker("Favorite Color",
+                           selection: $selectedChartsIndex,
+                           content: {
+                            Text("Artists").tag(0)
+                            Text("Tracks").tag(1)
+                           }).padding(.horizontal, 20)
+                        .padding(.vertical, 5)
+                        .pickerStyle(SegmentedPickerStyle())
+
+                    List(charts.artists) { artist in
+                        NavigationLink(
+                            destination: ArtistView(artist: artist),
+                            label: {
+                                ArtistRow(artist: artist)
+                            })
+                    }.navigationTitle("Global charts").onAppear {
+                        if !loaded {
+                            self.charts.getChartingArtists()
+                            // Prevent loading artits again when navigating
+                            self.loaded = true
+                        }
                     }
                 }
+                // Show loader above the rest of the ZStack
                 if charts.isLoading {
                     ProgressView()
                 }
             }
+
         }
     }
 }
