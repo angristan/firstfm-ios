863eb5632431e651b1a63ef7a80a296b6eba23f0
Stanislas <stanislas.lange@pm.me>
2021-07-09 17:46:43 +0200

    fix: image display on search list

--- firstfm/Data/ViewModel/SearchViewModel.swift
+++ firstfm/Data/ViewModel/SearchViewModel.swift
@@ -10,35 +10,46 @@ import Foundation
 class SearchViewModel: ObservableObject {
     @Published var artists: [Artist] = []
     var isLoading = false
-    
+
     func searchForArtist(artist: String) {
         self.isLoading = true
-        
+
         LastFMAPI.request(lastFMMethod: "artist.search", args: ["artist": artist]) { (data: ArtistSearchResponse?, error) -> Void in
             self.isLoading = false
-            
+
             if let data = data {
                 var artists = data.results.artistmatches.artist
-                
+
                 for (index, _) in artists.enumerated() {
                     if artists[index].image[0].url == "" {
                         artists[index].image[0].url = "https://lastfm.freetls.fastly.net/i/u/64s/4128a6eb29f94943c9d206c08e625904.webp"
                     }
                 }
-                
+
                 DispatchQueue.main.async {
                     self.artists = artists
                     // Let's stop the loader, the images will be loaded aynchronously
                     self.isLoading = false
                 }
-                
+
                 for (index, artist) in artists.enumerated() {
                     // Get image URL for each artist and trigger a View update through the observed object
                     ChartViewModel().getImageForArtist(artistName: artist.name) { imageURL in
                         if let imageURL = imageURL {
                             DispatchQueue.main.async {
                                 self.artists[index].image[0].url = imageURL
+
+                        for (index, artist) in artists.enumerated() {
+                            // Get image URL for each artist and trigger a View update through the observed object
+                            ChartViewModel().getImageForArtist(artistName: artist.name) { imageURL in
+                                if let imageURL = imageURL {
+                                    artists[index].image[0].url = imageURL
+                                }
                             }
+                        }
+
+                        DispatchQueue.main.async {
+                            self.artists = artists
                         }
                     }
                 }
