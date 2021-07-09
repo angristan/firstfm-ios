e1c1f631992481c1ac698a1c69acb3034d964229
quantumsheep <nathanael.dmc@outlook.fr>
2021-07-09 12:12:17 +0200

    refactor: use `LastFMAPI.request` in `UserViewModel`

--- firstfm/Data/ViewModel/UserViewModel.swift
+++ firstfm/Data/ViewModel/UserViewModel.swift
@@ -11,49 +11,17 @@ class ProfileViewModel: ObservableObject {
     @Published var user: User?
     var isLoading = true
 
-    // swiftlint:disable force_cast
-    let lastFMAPIKey = Bundle.main.object(forInfoDictionaryKey: "LastFMAPIKey") as! String
-
     func getProfile(username: String) {
         self.isLoading = true
-
-        var request = URLRequest(url: URL(string: "https://ws.audioscrobbler.com/2.0/?format=json")!)
-
-        let data : Data = "api_key=\(lastFMAPIKey)&method=user.getInfo&user=\(username)".data(using: .utf8)!
-
-        request.httpMethod = "POST"
-        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField:"Content-Type");
-        request.httpBody = data
-
-
-        URLSession.shared.dataTask(with: request) { (data, response, error) -> Void in
-            do {
-                if let response = response {
-                    let nsHTTPResponse = response as? HTTPURLResponse
-                    if let statusCode = nsHTTPResponse?.statusCode {
-                        print ("status code = \(statusCode)")
-                    }
-                    // TODO
-                }
-                if let error = error {
-                    print (error)
-                    // TODO
-                }
-                if let data = data {
-                    do{
-                        let jsonResponse = try JSONDecoder().decode(UserInfoResponse.self, from: data)
-
-                        DispatchQueue.main.async {
-                            self.user = jsonResponse.user
-                            self.isLoading = false
-                        }
-                    }
+        
+        LastFMAPI.request(method: "POST", lastFMMethod: "user.getInfo", config: ["user": username]) { (data: UserInfoResponse?, error) -> Void in
+            self.isLoading = false
+            
+            if let data = data {
+                DispatchQueue.main.async {
+                    self.user = data.user
                 }
             }
-            catch {
-                print(error)
-                // TODO
-            }
-        }.resume()
+        }
     }
 }
