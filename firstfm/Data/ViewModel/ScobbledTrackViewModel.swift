d77f2e0a076d99293194ba5bec0874d8c671a7d9
Stanislas <stanislas.lange@pm.me>
2021-07-17 22:41:37 +0200

    feat(scrobbles): add like button on tracks

---
+++ firstfm/Data/ViewModel/ScobbledTrackViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  ScobbledTrackViewModel.swift
+//  firstfm
+//
+//  Created by Stanislas Lange on 17/07/2021.
+//
+
+import Foundation
+import SwiftUI
+import NotificationBannerSwift
+
+struct Nothing: Codable {}
+
+
+class ScrobbledTrackViewModel {
+    @AppStorage("lastfm_sk") var storedToken: String?
+    
+    func loveTrack(track: ScrobbledTrack) {
+        LastFMAPI.request(lastFMMethod: "track.love", args: ["artist": track.artist.name, "track": track.name, "sk": storedToken ?? ""]) { (data: Nothing?, error) -> Void in
+            if error != nil {
+                DispatchQueue.main.async {
+                    FloatingNotificationBanner(title: "Failed to like track", subtitle: error?.localizedDescription, style: .danger).show()
+                }
+            }
+        }
+    }
+    
+    func unloveTrack(track: ScrobbledTrack) {
+        LastFMAPI.request(lastFMMethod: "track.unlove", args: ["artist": track.artist.name, "track": track.name, "sk": storedToken ?? ""]) { (data: Nothing?, error) -> Void in
+            if error != nil {
+                DispatchQueue.main.async {
+                    FloatingNotificationBanner(title: "Failed to unlike track", subtitle: error?.localizedDescription, style: .danger).show()
+                }
+            }
+        }
+    }
+}
--- firstfm/Views/SubViews/ScrobbledTrackRowView.swift
+++ firstfm/Views/SubViews/ScrobbledTrackRowView.swift
@@ -9,39 +9,60 @@ import SwiftUI
 import Kingfisher
 
 struct ScrobbledTrackRow: View {
-    var track: ScrobbledTrack
+    @State var track: ScrobbledTrack
+    var vm = ScrobbledTrackViewModel()
     
     var body: some View {
-        KFImage.url(URL(string: track.image[3].url )!)
-            .resizable()
-            .onSuccess { res in
-                print("Success: \(self.track.name) - \(res.cacheType)")
-            }
-            .onFailure { err in
-                print("Error \(self.track.name): \(err)")
-            }
-            .fade(duration: 1)
-            .cancelOnDisappear(true)
-            .cornerRadius(5)
-            .frame(width: 50, height: 50)
-        
-        
-        VStack(alignment: .leading) {
-            Spacer()
-            HStack {
-                Text(track.name)
-                    .font(.headline)
-                    .lineLimit(1)
+        HStack {
+            KFImage.url(URL(string: track.image[3].url )!)
+                .resizable()
+                .onSuccess { res in
+                    print("Success: \(self.track.name) - \(res.cacheType)")
+                }
+                .onFailure { err in
+                    print("Error \(self.track.name): \(err)")
+                }
+                .fade(duration: 1)
+                .cancelOnDisappear(true)
+                .cornerRadius(5)
+                .frame(width: 60, height: 60)
+            
+            
+            VStack(alignment: .leading) {
                 Spacer()
-                Text(track.date?.getRelative() ?? "Now Playing")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-                    .frame(alignment: .leading)
+                HStack {
+                    Text(track.name)
+                        .font(.headline)
+                        .lineLimit(1)
+                }
+                Spacer()
+                HStack {
+                    Text(track.artist.name)
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                    Spacer()
+                    Text(track.date?.getRelative() ?? "Now Playing")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                        .frame(alignment: .leading)
+                }
             }
-            Spacer()
-            Text(track.artist.name)
-                .font(.subheadline)
-                .foregroundColor(.gray)
+            Button(action: {
+                if self.track.loved == "0" {
+                    self.track.loved = "1"
+                    vm.loveTrack(track: self.track)
+                } else {
+                    self.track.loved = "0"
+                    vm.unloveTrack(track: self.track)
+                }
+            })
+            {
+                if self.track.loved == "1" {
+                    Image(systemName: "heart.fill").imageScale(.large).foregroundColor(Color(red: 157, green: 0, blue: 0))
+                } else {
+                    Image(systemName: "heart").imageScale(.large)
+                }
+            }.buttonStyle(PlainButtonStyle())
         }
     }
 }
