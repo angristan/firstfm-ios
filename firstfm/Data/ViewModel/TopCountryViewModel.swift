be7f84b1d3aa8adb96abd9b1113e3c9c3c9210b6
quantumsheep <nathanael.dmc@outlook.fr>
2021-07-18 03:33:11 +0200

    fix: fix code that I broke in the last commit

--- firstfm/Data/ViewModel/TopCountryViewModel.swift
+++ firstfm/Data/ViewModel/TopCountryViewModel.swift
@@ -22,7 +22,7 @@ class TopCountryViewModel: ObservableObject {
             if let data = data {
                 var artists = data.topartists.artist
 
-                for index in artists.indices() where artists[index].image[0].url == "" {
+                for index in artists.indices where artists[index].image[0].url == "" {
                     artists[index].image[0].url = "https://lastfm.freetls.fastly.net/i/u/64s/4128a6eb29f94943c9d206c08e625904.webp"
                 }
 
