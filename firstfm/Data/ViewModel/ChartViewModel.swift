ba228014f7d72acb0526d517b9bb8d1fd007dfd4
quantumsheep <nathanael.dmc@outlook.fr>
2021-07-18 03:33:11 +0200

    fix: fix code that I broke in the last commit

--- firstfm/Data/ViewModel/ChartViewModel.swift
+++ firstfm/Data/ViewModel/ChartViewModel.swift
@@ -34,7 +34,7 @@ class ChartViewModel: ObservableObject {
             if let data = data {
                 var artists = data.artists.artist
 
-                for index in artists.indices() where artists[index].image[0].url == "" {
+                for index in artists.indices where artists[index].image[0].url == "" {
                     artists[index].image[0].url = "https://lastfm.freetls.fastly.net/i/u/64s/4128a6eb29f94943c9d206c08e625904.webp"
                 }
 
--- firstfm/Data/ViewModel/SearchViewModel.swift
+++ firstfm/Data/ViewModel/SearchViewModel.swift
@@ -20,7 +20,7 @@ class SearchViewModel: ObservableObject {
             if let data = data {
                 var artists = data.results.artistmatches.artist
 
-                for index in artists.indices() where artists[index].image[0].url == "" {
+                for index in artists.indices where artists[index].image[0].url == "" {
                     artists[index].image[0].url = "https://lastfm.freetls.fastly.net/i/u/64s/4128a6eb29f94943c9d206c08e625904.webp"
                 }
 
