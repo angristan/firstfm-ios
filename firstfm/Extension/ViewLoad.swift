fbe01ae6062dc9dab609df3793c9fd8f5e3a639f
Stanislas <stanislas.lange@pm.me>
2021-07-24 01:56:13 +0200

    feat: add onLoad View extension

---
+++ firstfm/Extension/ViewLoad.swift
@@ -0,0 +1,31 @@
+import Foundation
+import SwiftUI
+
+// https://stackoverflow.com/a/64495887/6945353
+
+struct ViewDidLoadModifier: ViewModifier {
+    
+    @State private var didLoad = false
+    private let action: (() -> Void)?
+    
+    init(perform action: (() -> Void)? = nil) {
+        self.action = action
+    }
+    
+    func body(content: Content) -> some View {
+        content.onAppear {
+            if didLoad == false {
+                didLoad = true
+                action?()
+            }
+        }
+    }
+    
+}
+
+extension View {
+    func onLoad(perform action: (() -> Void)? = nil) -> some View {
+        modifier(ViewDidLoadModifier(perform: action))
+    }
+    
+}
