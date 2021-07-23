43b5869f11a273415263d015c0df8f1790e67725
Stanislas <stanislas.lange@pm.me>
2021-07-23 18:41:09 +0200

    feat(scrobbles): add nice logged out view

--- firstfm/Views/Scrobbles/ScrobblesView.swift
+++ firstfm/Views/Scrobbles/ScrobblesView.swift
@@ -2,38 +2,43 @@ import SwiftUI
 import SwiftUIRefresh
 
 struct Scrobbles: View {
+    @EnvironmentObject var auth: AuthViewModel
     @ObservedObject var vm = ScrobblesViewModel()
     @State var scrobblesLoaded = false
     @State private var isPullLoaderShowing = false
 
     var body: some View {
         NavigationView {
-            ZStack {
-                VStack {
-                    List(vm.scrobbles) { track in
-                        NavigationLink(
-                            destination: TrackView(track: Track(name: track.name, playcount: "0", listeners: "", url: "", artist: nil, image: track.image)),
-                            label: {
-                            ScrobbledTrackRow(track: track)
-                        })
-                    }.navigationTitle("Your scrobbles").onAppear {
-                        if !scrobblesLoaded {
+            if auth.isLoggedIn() {
+                ZStack {
+                    VStack {
+                        List(vm.scrobbles) { track in
+                            NavigationLink(
+                                destination: TrackView(track: Track(name: track.name, playcount: "0", listeners: "", url: "", artist: nil, image: track.image)),
+                                label: {
+                                    ScrobbledTrackRow(track: track)
+                                })
+                        }.navigationTitle("Your scrobbles").onAppear {
+                            if !scrobblesLoaded {
+                                vm.getUserScrobbles()
+                                // Prevent loading again when navigating
+                                scrobblesLoaded = true
+                            }
+                        }
+                        .pullToRefresh(isShowing: $isPullLoaderShowing) {
                             vm.getUserScrobbles()
-                            // Prevent loading again when navigating
-                            scrobblesLoaded = true
+                            self.isPullLoaderShowing = false
                         }
                     }
-                    .pullToRefresh(isShowing: $isPullLoaderShowing) {
-                        vm.getUserScrobbles()
-                        self.isPullLoaderShowing = false
+                    // Show loader above the rest of the ZStack
+                    if vm.isLoading {
+                        ProgressView().scaleEffect(2)
                     }
                 }
-                // Show loader above the rest of the ZStack
-                if vm.isLoading {
-                    ProgressView().scaleEffect(2)
-                }
-            }
 
+            } else {
+                SrobblesLoggedOutView()
+            }
         }
     }
 }
