8de19350508443af54a807410f07fbdd395a1751
Stanislas Lange <git@slange.me>
2022-07-07 20:04:01 +0200

    fix(artist): set mbid as optional

--- firstfm/Models/LastFM/ArtistInfo.swift
+++ firstfm/Models/LastFM/ArtistInfo.swift
@@ -4,7 +4,7 @@ struct ArtistInfo: Codable {
     let bio: ArtistBio
     let ontour: String
     let stats: ArtistStats
-    let mbid: String
+    let mbid: String?
     let image: [LastFMImage]
     var similar: SimilarArtists
     let url: String
