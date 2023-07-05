fca314b57d7e846c3728f3a752564f3f57ef2e3a
Stanislas Lange <git@slange.me>
2023-07-05 22:45:59 +0200

    fix(TopUserAlbumsView): fix array check syntax for new Swift version

--- firstfm/Views/Profile/TopUserAlbumsView.swift
+++ firstfm/Views/Profile/TopUserAlbumsView.swift
@@ -41,8 +41,8 @@ struct TopUserAlbumsView: View {
                                 destination: AlbumView(album: ArtistAlbum(mbid: album.mbid, name: album.name, playcount: UInt(album.playcount) ?? 0, url: album.url, image: album.image)),
                                 label: {
                                     VStack {
-                                        if let image = album.image {
-                                            KFImage.url(URL(string: image[0].url != "" ? image[0].url : "https://lastfm.freetls.fastly.net/i/u/64s/4128a6eb29f94943c9d206c08e625904.webp")!)
+                                        if !album.image.isEmpty {
+                                            KFImage.url(URL(string: album.image[0].url != "" ? album.image[0].url : "https://lastfm.freetls.fastly.net/i/u/64s/4128a6eb29f94943c9d206c08e625904.webp")!)
                                                     .resizable()
                                                     .cornerRadius(5)
                                                     .aspectRatio(contentMode: .fill)
